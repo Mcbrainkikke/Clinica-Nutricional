4262b7028e629c3af25176c0f454328f955f7770
Mcbrainkikke <victor9005@hotmail.com>
2023-11-02 11:59:53 -0500

    no se puede pedir cita antes de la fecha

--- frontend/src/componentes/Usuario/solicitudcita.jsx
+++ frontend/src/componentes/Usuario/solicitudcita.jsx
@@ -4,32 +4,9 @@ import axios from 'axios';
 const SolicitudCita = () => {
   const [fechaCita, setFechaCita] = useState('');
   const [horasDisponibles, setHorasDisponibles] = useState([]);
-  const [horaSeleccionada, setHoraSeleccionada] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const fechaActual = new Date().toISOString().split('T')[0];
 
-  // Función para agendar la cita
-  const handleAgendarCita = async (e) => {
-    e.preventDefault();
-
-    try {
-      if (fechaCita && horaSeleccionada) {
-        const confirmAgendar = window.confirm(`¿Desea agendar la cita para las ${horaSeleccionada}?`);
-        if (confirmAgendar) {
-          const response = await axios.post('/api/citas/agregar', {
-            fecha: fechaCita,
-            hora: horaSeleccionada
-          });
-          console.log(response.data); // Puedes manejar la respuesta del backend aquí
-          console.log(`Cita agendada para las ${horaSeleccionada} el día ${fechaCita}`);
-        }
-      } else {
-        alert('Seleccione una fecha y hora para agendar la cita');
-      }
-    } catch (error) {
-      console.error('Error al agendar la cita:', error);
-    }
-  };
-
-  // Función para obtener horas disponibles
   const obtenerHorasDisponibles = async () => {
     try {
       if (fechaCita) {
@@ -37,14 +14,13 @@ const SolicitudCita = () => {
           fecha: fechaCita
         });
         setHorasDisponibles(response.data.horasDisponibles);
+        setMensaje('');
       }
     } catch (error) {
       console.error('Error al obtener horas disponibles:', error);
     }
   };
 
-  const [mensaje, setMensaje] = useState('');
-  // Función para seleccionar la hora
   const handleSeleccionHora = async (hora) => {
     const confirmSeleccion = window.confirm(`¿Desea agendar la cita para las ${hora}?`);
     if (confirmSeleccion) {
@@ -53,9 +29,9 @@ const SolicitudCita = () => {
           fecha: fechaCita,
           hora: hora
         });
-        setMensaje('Cita agendada con éxito'); //mensaje en el frontend
-        console.log(response.data); // Manejar la respuesta del backend aquí
-        console.log(`Cita agendada para las ${hora} el día ${fechaCita}`);
+        setMensaje(`Cita agendada para las ${hora} el día ${fechaCita}`);
+        setHorasDisponibles([]);
+        console.log(response.data);
       } catch (error) {
         console.error('Error al agendar la cita:', error);
       }
@@ -68,56 +44,75 @@ const SolicitudCita = () => {
     }
   }, [fechaCita]);
 
+  const volver = () => {
+    setMensaje('');
+    setHorasDisponibles([]);
+  };
+
+  const validarFecha = (fecha) => {
+    return fecha >= fechaActual;
+  };
+
   return (
     <div className="container mt-5 navbar-dark">
       <h2 className='text-white'>Solicitud de Cita</h2>
-      <div className='row'>
-        <div className="col-md-3"></div>
-        <div className="col-md-6" style={{ marginBottom: '20px' }}>
-          <form onSubmit={handleAgendarCita}>
-            <div className="form-group" style={{ marginBottom: '20px' }}>
-              <label className="form-label text-white">Fecha Deseada</label>
-              <input
-                type="date"
-                className="form-control"
-                style={{ background: 'white', color: 'black', border: '1px solid white', padding: '5px' }}
-                value={fechaCita}
-                onChange={(e) => setFechaCita(e.target.value)}
-                placeholder="Fecha"
-              />
-            </div>
-            <br />
-            {/*<button type="submit" className='btn btn-outline-primary' style={{ backgroundColor: 'white' }}>Buscar Cita</button>*/}
-          </form>
+      { !mensaje &&
+        <div className='row'>
+          <div className="col-md-3"></div>
+          <div className="col-md-6" style={{ marginBottom: '20px' }}>
+            <form>
+              <div className="form-group" style={{ marginBottom: '20px' }}>
+                <label className="form-label text-white">Fecha Deseada</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  style={{ background: 'white', color: 'black', border: '1px solid white', padding: '5px' }}
+                  value={fechaCita}
+                  onChange={(e) => {
+                    if (validarFecha(e.target.value)) {
+                      setFechaCita(e.target.value);
+                    } else {
+                      alert('Fecha incorrecta, Seleccione una fecha mas reciente');
+                      setFechaCita('');
+                    }
+                  }}
+                  placeholder="Fecha"
+                />
+              </div>
+              <br />
+            </form>
+          </div>
         </div>
-      </div>
-      {fechaCita &&
-         <div className="row" style={{ marginBottom: '20px' }}>
-        <div className="col-md-3"></div>
-        <div className="col-md-6" style={{ background: 'white', textAlign: 'center', marginBottom: '20px' }}>
-          <h4>Horas Disponibles:</h4>
-          <ul style={{ listStyle: 'none', padding: 0 }}>
-            {horasDisponibles.map((hora, index) => (
-              <button
-                key={index}
-                onClick={() => handleSeleccionHora(hora)}
-                className='btn btn-outline-primary'
-                style={{ backgroundColor: 'white', marginBottom: '5px' }}
-              >
-                {hora}
-              </button>
-            ))}
-          </ul>
+      }
+      {fechaCita && !mensaje &&
+        <div className="row" style={{ marginBottom: '20px' }}>
+          <div className="col-md-3"></div>
+          <div className="col-md-6" style={{ background: 'white', textAlign: 'center', marginBottom: '20px' }}>
+            <h4>Horas Disponibles:</h4>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+              {horasDisponibles.map((hora, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleSeleccionHora(hora)}
+                  className='btn btn-outline-primary'
+                  style={{ backgroundColor: 'white', marginBottom: '5px' }}
+                >
+                  {hora}
+                </button>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
       }
       {mensaje && (
-      <div style={{ marginBottom: '20px', paddingBottom: '20px' }}>
-        <p className='text-white'>{mensaje}</p> {/* Mensaje */}
-      </div>
-    )}
+        <div style={{ marginBottom: '20px', paddingBottom: '20px' }}>
+          <p className='text-white'>{mensaje}</p>
+          <button onClick={volver} className="btn btn-primary">Volver</button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default SolicitudCita;
+
